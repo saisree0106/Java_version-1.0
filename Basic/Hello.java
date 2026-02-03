3a07d5c8438f215b95301897c75c8e304b7015a2
saisree0106 <saisree011106@gmail.com>
2026-02-03 18:34:50 +0530

    first

--- Basic/Hello.java
+++
@@ -1,6 +0,0 @@
-public class Hello {
-     public static void main(String[] var0) {
-      System.out.println("Hello");
-      System.out.println("BalajiReddy");
-   }
-}
--- Basic/Hello2.java
+++
@@ -1,6 +0,0 @@
-public class Hello2 {
-     public static void main(String[] var0) {
-      System.out.println("Hello");
-      System.out.println("BalajiReddy");
-   }
-}
